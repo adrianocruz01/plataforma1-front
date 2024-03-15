7c1ef5aaf2c2473dd06e6da18fe604553bb3f2f3
Cayro Cerqueira <cayro.cerqueira@grancursosonline.com.br>
2024-03-14 22:00:31 -0300

    first commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,118 +1,211 @@
+import { useState, useEffect } from "react";
 import Image from "next/image";
-import { Inter } from "next/font/google";
+import Pencil from "../assets/images/pencil.svg";
+import Folder from "../assets/images/folder.svg";
+import XMark from "../assets/images/x-mark.svg";
+import Trash from "../assets/images/trash.svg";
 
-const inter = Inter({ subsets: ["latin"] });
+const TasksPage = () => {
+  const [tasks, setTasks] = useState([]);
+  const [newTaskText, setNewTaskText] = useState("");
+  const [newTaskImgURL, setNewTaskImgURL] = useState("");
+  const [editingTaskId, setEditingTaskId] = useState(null);
 
-export default function Home() {
+  useEffect(() => {
+    fetchTasks();
+  }, []);
+
+  const fetchTasks = async () => {
+    try {
+      const response = await fetch(
+        "https://zurytreinamento-11df2808e851.herokuapp.com/listar-treinamentos"
+      );
+      const data = await response.json();
+      setTasks(data.data);
+    } catch (error) {
+      console.error("Erro ao buscar tarefas:", error);
+    }
+  };
+
+  const createTask = async () => {
+    if (!newTaskText) return;
+
+    try {
+      const response = await fetch(
+        "https://zurytreinamento-11df2808e851.herokuapp.com/novo-treinamento",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            description: newTaskText,
+            image: newTaskImgURL === "" ? null : newTaskImgURL,
+          }),
+        }
+      );
+      const data = await response.json();
+      setTasks([...tasks, data]);
+      setNewTaskText("");
+    } catch (error) {
+      console.error("Erro ao criar tarefa:", error);
+    }
+  };
+
+  const deleteTask = async (taskId) => {
+    try {
+      await fetch(
+        `https://zurytreinamento-11df2808e851.herokuapp.com/deletar-treinamento/${taskId}`,
+        {
+          method: "DELETE",
+        }
+      );
+      setTasks(tasks.filter((task) => task.id !== taskId));
+    } catch (error) {
+      console.error("Erro ao deletar tarefa:", error);
+    }
+  };
+
+  const editTask = async (taskId) => {
+    if (!newTaskText) return;
+
+    try {
+      const response = await fetch(
+        `https://zurytreinamento-11df2808e851.herokuapp.com/editar-treinamento/${taskId}`,
+        {
+          method: "PUT",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            description: newTaskText,
+            image: newTaskImgURL === "" ? null : newTaskImgURL,
+          }),
+        }
+      );
+      const data = await response.json();
+      setTasks(tasks.map((task) => (task.id === taskId ? data : task)));
+      setEditingTaskId(null);
+    } catch (error) {
+      console.error("Erro ao editar tarefa:", error);
+    }
+  };
+
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/pages/index.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
+    <div className="flex justify-center min-h-screen w-full">
+      <div className="px-4 mb-10 max-w-3xl w-full">
+        <h1 className="text-2xl font-semibold mt-16 mb-8">Treinamentos</h1>
+        <div className="border border-zinc-300 px-10 py-8 flex flex-col rounded-2xl h-fit max-w-4xl min-w-full">
+          <div className="flex gap-3 mb-6 flex-col">
+            <input
+              type="text"
+              value={newTaskText}
+              onChange={(e) => setNewTaskText(e.target.value)}
+              placeholder="Descreva o treinamento"
+              className="h-9 rounded-md p-1 focus-visible:outline-none border border-zinc-300 focus-visible:border-zinc-600"
             />
-          </a>
+            <input
+              type="url"
+              value={newTaskImgURL}
+              onChange={(e) => setNewTaskImgURL(e.target.value)}
+              placeholder="URL da imagem (opicional)"
+              className="h-9 rounded-md p-1 focus-visible:outline-none border border-zinc-300 focus-visible:border-zinc-600"
+            />
+            <button
+              className="self-start px-3 py-2 bg-purple-600 text-white rounded-md font-medium shadow"
+              onClick={createTask}
+            >
+              Adicionar treinamento
+            </button>
+          </div>
+          <ul>
+            {tasks.map((task) => (
+              <li
+                key={task.id}
+                className="border border-zinc-300 p-4 rounded-md mb-2 last:mb-0 flex justify-between flex-col md:flex-row"
+              >
+                {editingTaskId === task.id ? (
+                  <div className="flex gap-3 flex-col lg:flex-row">
+                    <input
+                      type="text"
+                      value={newTaskText}
+                      onChange={(e) => setNewTaskText(e.target.value)}
+                      placeholder="Descreva o novo treinamento"
+                      className="h-9 rounded-md p-1 focus-visible:outline-none border border-zinc-300 focus-visible:border-zinc-600"
+                    />
+                    <input
+                      type="url"
+                      value={newTaskImgURL}
+                      onChange={(e) => setNewTaskImgURL(e.target.value)}
+                      placeholder="Nova URL da imagem (opicional)"
+                      className="h-9 rounded-md p-1 focus-visible:outline-none border border-zinc-300 focus-visible:border-zinc-600"
+                    />
+                  </div>
+                ) : (
+                  <div className="h-9 max-w-sm text-ellipsis overflow-hidden whitespace-nowrap">
+                    {task.description}
+                  </div>
+                )}
+                {editingTaskId === task.id ? (
+                  <div className="flex gap-1 mt-3">
+                    <button
+                      className="p-2 rounded-full hover:bg-zinc-200 transition-colors duration-300"
+                      onClick={() => editTask(task.id, newTaskText)}
+                    >
+                      <Image
+                        src={Folder}
+                        width={18}
+                        height={18}
+                        style={{ minWidth: "18px" }}
+                      />
+                    </button>
+                    <button
+                      className="p-2 rounded-full hover:bg-zinc-200 transition-colors duration-300"
+                      onClick={() => setEditingTaskId(null)}
+                    >
+                      <Image
+                        src={XMark}
+                        width={18}
+                        height={18}
+                        style={{ minWidth: "18px" }}
+                      />
+                    </button>
+                  </div>
+                ) : (
+                  <div className="flex gap-1 w-max">
+                    <button
+                      className="p-2 rounded-full hover:bg-zinc-200 transition-colors duration-300"
+                      onClick={() => setEditingTaskId(task.id)}
+                    >
+                      <div>
+                        <Image
+                          src={Pencil}
+                          width={18}
+                          height={18}
+                          style={{ minWidth: "18px" }}
+                        />
+                      </div>
+                    </button>
+                    <button
+                      className="p-2 rounded-full hover:bg-zinc-200 transition-colors duration-300"
+                      onClick={() => deleteTask(task.id)}
+                    >
+                      <Image
+                        src={Trash}
+                        width={18}
+                        height={18}
+                        style={{ minWidth: "18px" }}
+                      />
+                    </button>
+                  </div>
+                )}
+              </li>
+            ))}
+          </ul>
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </div>
   );
-}
+};
+
+export default TasksPage;
