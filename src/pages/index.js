0dea2b7b6ed8e61ac8f891ae185f5662d473b3ae
Adriano Cruz <94023985+adrianocruz01@users.noreply.github.com>
2024-05-31 22:05:41 -0300

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -18,7 +18,7 @@ const TasksPage = () => {
   const fetchTasks = async () => {
     try {
       const response = await fetch(
-        "https://treinamento-back-bba33a6c4f46.herokuapp.com/listar-treinamentos"
+        "https://treinamento-back-52d6b8a90d66.herokuapp.com/listar-treinamentos"
       );
       const data = await response.json();
       setTasks(data.data);
@@ -32,7 +32,7 @@ const TasksPage = () => {
 
     try {
       const response = await fetch(
-        "https://treinamento-back-bba33a6c4f46.herokuapp.com/novo-treinamento",
+       "https://treinamento-back-52d6b8a90d66.herokuapp.com/ovo-treinamento",
         {
           method: "POST",
           headers: {
