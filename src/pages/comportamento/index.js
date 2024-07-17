17c96c839c8028ae53c70776f3c77f1c0183100d
Adriano Cruz <94023985+adrianocruz01@users.noreply.github.com>
2024-07-17 14:41:42 -0300

    Update index.js

--- src/pages/comportamento/index.js
+++ src/pages/comportamento/index.js
@@ -10,7 +10,7 @@ const TrainingsPage = () => {
             </div>
             <Nav page="configuracoes"  />
             <div className="px-6 md:px-10 w-full bg-animate">
-                <h1 className="text-3xl font-bold mt-16 mb-8">Comportamento</h1>
+                <h1 className="text-3xl font-bold mt-16 mb-8">Personalidade</h1>
                 <div className="max-w-5xl">
                     <div className="bg-white rounded-2xl shadow-lg p-8">
                     <Behavior />
