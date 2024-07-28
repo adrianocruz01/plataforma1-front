72e924ae33d7298b4c60510c98875f259f8f401b
cayrota <cayropraca@gmail.com>
2024-07-27 21:34:45 -0300

    ajusta os balões de mensagem

--- src/components/Message/index.js
+++ src/components/Message/index.js
@@ -1,17 +1,13 @@
 import Image from "next/image";
+import React from "react";
 
 const Message = ({ message }) => {
+    const isAssistant = message.role === "assistant";
     return (
-        <div
-            className={`${
-                message.role === "assistant"
-                    ? "bg-cyan-600 ml-auto"
-                    : "bg-zinc-400"
-            } max-w-[340px] w-fit p-5 flex gap-3 rounded-xl shadow-lg flex-col text-white overflow-wrap-anywhere`}
-        >
+        <div className={`relative max-w-[340px] w-fit p-3 md:p-5 flex gap-3 shadow-lg flex-col text-white break-words ${isAssistant ? 'bg-cyan-600 ml-auto rounded-t-xl rounded-bl-xl' : 'bg-zinc-400 rounded-t-xl rounded-br-xl'}`}>
             {message.audios.length ? (
                 <div className="flex flex-col gap-4">
-                    <audio controls>
+                    <audio controls className="max-w-full">
                         <source src={message.audios[0]} type="audio/ogg" />
                         Your browser does not support the audio element.
                     </audio>
@@ -35,6 +31,7 @@ const Message = ({ message }) => {
             ) : (
                 ""
             )}
+            <div className={`absolute -bottom-2 ${isAssistant ? '-right-2 border-l-8 border-l-transparent border-y-8 border-y-transparent border-r-8 border-r-cyan-600 -rotate-45' : '-left-2 border-r-8 border-r-transparent border-y-8 border-y-transparent border-l-8 border-l-zinc-400 rotate-45'}`}></div>
         </div>
     );
 };
