81667f4dcf87e22f613b606ab02aaaf6dd6583c7
cayrota <cayropraca@gmail.com>
2024-07-29 15:41:10 -0300

    adiciona favicon e title

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,9 +1,13 @@
 import Layout from "@/components/Layout";
 import "@/styles/globals.css";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }) {
     return (
         <Layout>
+            <Head>
+                <title>Zury AI</title>
+            </Head>
             <Component {...pageProps} />
         </Layout>
     );
